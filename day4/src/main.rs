f957098da638f090e7617cdf68b73b2e3cc97ebe
Rutger Moons <rutgermoons1@hotmail.com>
2020-12-05 11:42:33 +0100

    improve day 4 with regex validation

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -2,67 +2,49 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 use regex::Regex;
 
-fn solve(passports : &Vec<String>) -> u32 {
+fn solve(passports: &Vec<String>) -> u32 {
     let mut nb_valid = 0;
+    let re_byr = Regex::new(r"byr:").unwrap();
+    let re_iyr = Regex::new(r"iyr:").unwrap();
+    let re_eyr = Regex::new(r"eyr:").unwrap();
+    let re_hgt = Regex::new(r"hgt:").unwrap();
+    let re_hcl = Regex::new(r"hcl:").unwrap();
+    let re_ecl = Regex::new(r"ecl:").unwrap();
+    let re_pid = Regex::new(r"pid:").unwrap();
+
     for pass in passports {
-        let mut byr = 0;
-        let re_byr = Regex::new(r"byr:(\d{4})").unwrap();
-        for cap in re_byr.captures_iter(pass) {
-            byr = cap[1].parse().unwrap();
-        }
-        if byr < 1920 || byr > 2002 { continue ; }
+        if re_byr.is_match(pass) &&
+           re_iyr.is_match(pass) &&
+           re_eyr.is_match(pass) &&
+           re_hgt.is_match(pass) &&
+           re_hcl.is_match(pass) &&
+           re_ecl.is_match(pass) &&
+           re_pid.is_match(pass) {
+               nb_valid += 1;
+           }
+    }
+    nb_valid
+}
 
-        let mut iyr = 0;
-        let re_iyr = Regex::new(r"iyr:(\d{4})").unwrap();
-        for cap in re_iyr.captures_iter(pass) {
-            iyr = cap[1].parse().unwrap();
-        }
-        if iyr < 2010 || iyr > 2020 { continue ; }
-
-        let mut eyr = 0;
-        let re_eyr = Regex::new(r"eyr:(\d{4})").unwrap();
-        for cap in re_eyr.captures_iter(pass) {
-            eyr = cap[1].parse().unwrap();
-        }
-        if eyr < 2020 || eyr > 2030 { continue ; }
-
-
-        let mut hgt = 0;
-        let mut hgt_unit = "".to_owned();
-        let re_hgt = Regex::new(r"hgt:(\d{2,3})(\w{2})").unwrap();
-        for cap in re_hgt.captures_iter(pass) {
-            hgt = cap[1].parse().unwrap();
-            hgt_unit = cap[2].to_string();
-        }
-        match hgt_unit.as_str() {
-            "cm" => if hgt < 150 || hgt > 193 { continue ; },
-            "in" => if hgt < 59 || hgt > 76 { continue ; },
-            _ => { continue ; },
-        }
-        
-        let mut hcl = "".to_owned();
-        let re_hcl = Regex::new(r"hcl:(#[0-9a-f]{6})").unwrap();
-        for cap in re_hcl.captures_iter(pass) {
-            hcl = cap[1].to_string();
-        }
-        if hcl == "" { continue ; }
-
-        let mut ecl = "".to_owned();
-        let re_ecl = Regex::new(r"ecl:(amb|blu|brn|gry|grn|hzl|oth)").unwrap();
-        for cap in re_ecl.captures_iter(pass) {
-            ecl = cap[1].to_string();
-        }
-        if ecl == "" { continue ; }
-
-        let mut pid = "".to_owned();
-        let re_pid = Regex::new(r"pid:(\d*)").unwrap();
-        for cap in re_pid.captures_iter(pass) {
-            pid = cap[1].to_string();
-        }
-        let leng = pid.chars().count();
-        if pid == "" || leng != 9 { continue ; }
-
-        nb_valid += 1;
+fn solve_part_2(passports : &Vec<String>) -> u32 {
+    let mut nb_valid = 0;
+    let re_byr = Regex::new(r"byr:(19[2-9][0-9]|200[0-2])\b").unwrap();
+    let re_iyr = Regex::new(r"iyr:20(1[0-9]|20)\b").unwrap();
+    let re_eyr = Regex::new(r"eyr:20(2[0-9]|30)\b").unwrap();
+    let re_hgt = Regex::new(r"hgt:((59|6[0-9]|7[0-6])in|(1[5-8][0-9]|19[0-3])cm)\b").unwrap();
+    let re_hcl = Regex::new(r"hcl:#[0-9a-f]{6}\b").unwrap();
+    let re_ecl = Regex::new(r"ecl:(amb|blu|brn|gry|grn|hzl|oth)\b").unwrap();
+    let re_pid = Regex::new(r"pid:\d{9}\b").unwrap();
+    for pass in passports {
+        if re_byr.is_match(pass) &&
+           re_iyr.is_match(pass) &&
+           re_eyr.is_match(pass) &&
+           re_hgt.is_match(pass) &&
+           re_hcl.is_match(pass) &&
+           re_ecl.is_match(pass) &&
+           re_pid.is_match(pass) {
+               nb_valid += 1;
+           }
     }
     nb_valid
 }
@@ -85,12 +67,10 @@ fn main() -> io::Result<()> {
     }
 
     let result = solve(&passports);
-    println!("Result of part 2 is {}", result);
+    println!("Result of part 1 is {}", result);
 
-    /*
-    let result = solve_part_2(&landscape);
+    let result = solve_part_2(&passports);
     println!("Result of part 2 is {}", result);
-    */
 
     Ok(())
 }
