0caba3b1a44b714036afc567f34493d6c21faade
Rutger Moons <rutgermoons1@hotmail.com>
2020-12-15 18:07:50 +0100

    solve day 15 part 2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -7,7 +7,7 @@ fn speak_nr(last_spoken: u64, spoken: &mut HashMap<u64, u64>, turn: u64) -> u64 
     }
 }
 
-fn solve_part_1(nrs: &Vec<u64>) -> u64 {
+fn solve_part_1(nrs: &Vec<u64>, max_turn: u64) -> u64 {
     let mut spoken = HashMap::new();
     let mut turn = 0;
     let mut last_spoken: u64 = 0;
@@ -18,7 +18,7 @@ fn solve_part_1(nrs: &Vec<u64>) -> u64 {
         last_spoken = *nr;
     }
 
-    while turn < 2020 {
+    while turn < max_turn {
         last_spoken = speak_nr(last_spoken, &mut spoken, turn);
         turn += 1;
     }
@@ -32,8 +32,11 @@ fn main() {
                                 .map(|x| x.parse().unwrap())
                                 .collect();
 
-    let result = solve_part_1(&nrs);
+    let result = solve_part_1(&nrs, 2020);
     println!("Part 1: {}", result);
+
+    let result = solve_part_1(&nrs, 30_000_000);
+    println!("Part 2: {}", result);
 }
 
 #[cfg(test)]
@@ -43,36 +46,36 @@ mod tests {
 
     #[test]
     fn test_1() {
-        assert_eq!(solve_part_1(&vec![0,3,6]), 436);
+        assert_eq!(solve_part_1(&vec![0,3,6], 2020), 436);
     }
 
     #[test]
     fn test_2() {
-        assert_eq!(solve_part_1(&vec![1,3,2]), 1);
+        assert_eq!(solve_part_1(&vec![1,3,2], 2020), 1);
     }
 
     #[test]
     fn test_3() {
-        assert_eq!(solve_part_1(&vec![2,1,3]), 10);
+        assert_eq!(solve_part_1(&vec![2,1,3], 2020), 10);
     }
 
     #[test]
     fn test_4() {
-        assert_eq!(solve_part_1(&vec![2,3,1]), 78);
+        assert_eq!(solve_part_1(&vec![2,3,1], 2020), 78);
     }
 
     #[test]
     fn test_5() {
-        assert_eq!(solve_part_1(&vec![3,2,1]), 438);
+        assert_eq!(solve_part_1(&vec![3,2,1], 2020), 438);
     }
 
     #[test]
     fn test_6() {
-        assert_eq!(solve_part_1(&vec![1,2,3]), 27);
+        assert_eq!(solve_part_1(&vec![1,2,3], 2020), 27);
     }
 
     #[test]
     fn test_7() {
-        assert_eq!(solve_part_1(&vec![3,1,2]), 1836);
+        assert_eq!(solve_part_1(&vec![3,1,2], 2020), 1836);
     }
 }
