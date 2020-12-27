71fc27be31ea72dda4842983c7eb75658dd4cf6f
Rutger Moons <rutgermoons1@hotmail.com>
2020-12-27 19:49:06 +0100

    solve day 21 part 2

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -7,7 +7,7 @@ use std::collections::HashSet;
 type Food = (HashSet<String>, Vec<String>);
 type AllMap = HashMap<String, HashSet<String>>;
 
-fn solve_part_1(foods: &Vec<Food>) -> usize {
+fn get_allergen_map(foods: &Vec<Food>) -> AllMap {
     let mut allergen_map: AllMap = HashMap::new();
     for (ings, all) in foods.iter().cloned() {
         for allergen in all {
@@ -22,6 +22,11 @@ fn solve_part_1(foods: &Vec<Food>) -> usize {
             }
         }
     }
+    allergen_map
+}
+
+fn solve_part_1(foods: &Vec<Food>) -> usize {
+    let allergen_map = get_allergen_map(foods);
 
     // reduce to candidates only, now find the none-candidates
     let mut cand_ings: HashSet<String> = HashSet::new();
@@ -43,6 +48,52 @@ fn solve_part_1(foods: &Vec<Food>) -> usize {
     counter
 }
 
+fn solve_part_2(foods: &Vec<Food>) -> String {
+    let mut allergen_map = get_allergen_map(foods);
+
+    let mut ing_map: HashMap<String, String> = HashMap::with_capacity(allergen_map.len());
+    for (allergen, ingredients) in allergen_map.clone().iter() {
+        if ingredients.len() == 1 {
+            ing_map.insert(ingredients.iter().cloned().take(1).next().unwrap(), allergen.to_string());
+            allergen_map.remove(allergen);
+        }
+    }
+    let mut complete: bool = false;
+
+    while ! complete {
+        complete = true;
+        for (allergen, ingredients) in allergen_map.clone().iter() {
+            let ing_list: Vec<String> = ingredients.iter().cloned().collect();
+            for ing in ing_list {
+                if ing_map.contains_key(&ing) {
+                    allergen_map.get_mut(allergen).unwrap().remove(&ing);
+                }
+            }
+
+            let good = ingredients.len() == 1;
+            if good {
+                ing_map.insert(ingredients.iter().cloned().take(1).next().unwrap(), allergen.to_string());
+                allergen_map.remove(allergen);
+            } else {
+                complete = false;
+            }
+        }
+    }
+
+    let mut sorted: Vec<(&str, &str)> = ing_map.iter().map(|(x, y)| (y.as_str(), x.as_str())).collect();
+    sorted.sort();
+
+    let mut result: String = String::new();
+    for (_allergen, ing) in sorted {
+        if result.len() > 0 {
+            result.push(',');
+        }
+        result.push_str(ing);
+    }
+
+    result
+}
+
 fn main() -> io::Result<()> {
     let file = File::open("/home/rutger/old_home/arch-rutger/Programming/advent_of_code_2020/input/day21.txt")?;
     let reader = BufReader::new(file);
@@ -61,6 +112,9 @@ fn main() -> io::Result<()> {
     
     let result = solve_part_1(&foods);
     println!("Result of part 1 is {}", result);
+
+    let result = solve_part_2(&foods);
+    println!("Result of part 2 is {}", result);
 
     Ok(())
 }
